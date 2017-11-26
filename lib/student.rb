56a6fae39d4559d8a9b127989b613ed5a4b4a677
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-26 19:27:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,8 @@ class Student
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
-
+        name TEXT,
+        grade INTEGER
       )
     SQL
     DB[:conn].execute(sql)
